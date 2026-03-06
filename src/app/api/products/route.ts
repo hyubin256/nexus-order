21e5851934640dfd3ed09cfc30d48fc1bb766335
Huy Nguyen Minh <nguyenminhhuy@MacBook-Pro-cua-Nguyen.local>
2026-03-06 14:08:32 +0700

    Fix: Prisma

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from "next/server";
 import { ProductService } from "@/services/product.service";
 import { prisma } from "@/lib/prisma";
 
+export const dynamic = "force-dynamic";
+
 export async function GET(request: Request) {
   try {
     const { searchParams } = new URL(request.url);
